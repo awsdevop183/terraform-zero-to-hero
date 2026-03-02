b74ed337b9804cbfb6c41dad7dfd9b410273c174
Madhukar Reddy <madhu@madhukarreddy.com>
2026-03-02 15:00:53 +0530

    add local resouce

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
 resource "aws_vpc" "prod-vpc" {
     cidr_block = "192.168.0.0/16"  
+    tags_all = {
+      Name = "terraform-vpc"
+    }
+    tags = {
+        Name = "terraform-vpc"
+    }
   
 }
 resource "local_file" "localfile" {
     filename = "terraform.txt"
     content = "Hello"
+    file_permission = "777"
 }
 
 
@@ -15,7 +22,7 @@ resource "local_file" "localfile" {
 # file = resource_type
 # cidr_block = argument
 
-
+# prod-vpc = resource name
 # aws = provider
 # vpc = resource_type
 
@@ -23,3 +30,18 @@ resource "local_file" "localfile" {
 # filename and content = argument
 
 
+# immutable = you can modify
+# mutable = you can modify
+# # 
+
+
+# Terraform workflow
+
+# 1. write configuration files
+# 2. Terraform init
+# 3. Terraform plan
+# 4. terraform apply
+
+
+
+# terraform destroy 
