017418e626244e631c86afd963bffc960fb6ea62
Madhukar Reddy <madhu@madhukarreddy.com>
2026-03-10 16:24:21 +0530

    Add state locking

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "aws365.shop-remote"
     key    = "tfstate"
     region = "us-east-1"
+    dynamodb_table = "state-lock" 
   }
 }
--- main.tf
+++ main.tf
@@ -76,6 +76,16 @@ resource "aws_s3_bucket" "remote-state" {
   bucket = "aws365.shop-remote"
   
 }
+resource "aws_dynamodb_table" "terraform_locks" {
+  name           = "state-lock" # Unique name for your lock table
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 
 
 
